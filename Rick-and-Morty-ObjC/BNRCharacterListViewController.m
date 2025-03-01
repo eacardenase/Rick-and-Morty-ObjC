894ce46a6c7fa82402a99a26e98fa5a0c4d48eae
Edwin Cardenas <ecardenas@lwolf.com>
2025-03-01 18:35:41 -0500

    feat: fetch characters on view controller initialization

--- Rick-and-Morty-ObjC/BNRCharacterListViewController.m
+++ Rick-and-Morty-ObjC/BNRCharacterListViewController.m
@@ -7,15 +7,50 @@
 
 #import "BNRCharacterListViewController.h"
 
+@interface BNRCharacterListViewController ()
+
+@property (nonatomic) NSURLSession *session;
+
+@end
+
 @implementation BNRCharacterListViewController
 
+- (void)fetchCharacters
+{
+    NSString *requestString = @"https://rickandmortyapi.com/api/character";
+    NSURL *url = [NSURL URLWithString:requestString];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    
+    NSURLSessionDataTask *dataTask = [self.session dataTaskWithRequest:request completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSString *json = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+        
+        NSLog(@"%@", json);
+    }];
+    
+    [dataTask resume];
+}
+
 #pragma mark - Lifecycle
+
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    if (self = [super initWithStyle:style]) {
+        self.navigationItem.title = @"Rick and Morty Characters";
+        
+        NSURLSessionConfiguration *config = NSURLSessionConfiguration.defaultSessionConfiguration;
+        _session = [NSURLSession sessionWithConfiguration:config];
+        
+        [self fetchCharacters];
+    }
+    
+    return self;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    self.view.backgroundColor = UIColor.systemPinkColor;
+    self.view.backgroundColor = UIColor.whiteColor;
 }
 
 #pragma mark - UITableViewDataSource
